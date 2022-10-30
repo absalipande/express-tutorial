abd7668fb666ac6d5f606e205ebf5c04f95b841b
Amiel Brencis Salipande <100668029+absalipande@users.noreply.github.com>
2022-10-30 16:07:10 +0800

    Update expressTwo.js

--- expressTwo.js
+++ expressTwo.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 
 const users = [];
@@ -25,31 +26,35 @@ app.post('/users', (request, response) => {
     return response.status(400).json({
       error: 'Email is incorrect',
     });
-  } else {
-    const findEmail = users.find((user) => user.email === email);
-    if (findEmail) {
-      response.status(400).json({
-        error: 'Email already exists',
-      });
-    } else {
-      const filtered = Object.fromEntries(
-        Object.entries(data).filter(
-          ([key]) =>
-            key.includes('firstName') ||
-            key.includes('lastName') ||
-            key.includes('email')
-        )
-      );
-      const newUser = {
-        id: uuidv4(),
-        ...filtered,
-      };
-      users.push(newUser);
-      return response.status(201).json(newUser);
-    }
   }
+
+  const findEmail = users.find((user) => user.email === email);
+
+  if (findEmail) {
+    return response.status(400).json({
+      error: 'Email already exists',
+    });
+  }
+
+  const filteredData = Object.fromEntries(
+    Object.entries(data).filter(
+      ([key]) =>
+        key.includes('firstName') ||
+        key.includes('lastName') ||
+        key.includes('email')
+    )
+  );
+
+  const newUser = {
+    id: uuidv4(),
+    ...filteredData,
+  };
+
+  users.push(newUser);
+  response.status(201).json(newUser);
 });
 
 app.listen(port, () =>
   console.log(`Server Running on port: http://localhost/${port}`)
 );
+
